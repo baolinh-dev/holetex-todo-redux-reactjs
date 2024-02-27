fa8cda731ec0792e0c6614dc626e6d26f52d7552
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2024-02-28 02:27:47 +0700

    split reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,33 +1,11 @@
-const initState = {
-  filters: {
-    search: "",
-    status: "All",
-    priority: [],
-  },
-  todoList: [
-    { id: 1, name: "Learn Yoga", completed: false, priority: "Medium" },
-    { id: 2, name: "Learn Yoga 2", completed: true, priority: "High" },
-    { id: 3, name: "Learn Yoga 3", completed: false, priority: "Low" },
-  ],
-};
+import filtersReducer from "../components/Filters/FiltersSlice";
+import todoListReducer from "../components/Todo/TodosSlice";
 
-const rootReducer = (state = initState, action) => {
-  switch (action.type) {
-    case "todoList/addTodo":
-      return {
-        ...state,
-        todoList: [...state.todoList, action.payload],
-      };
-    case "filters/searchFilterChange":
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          search: action.payload,
-        },
-      };
-    default:
-      return state;
+
+const rootReducer = (state = {}, action) => {
+  return {
+    filters: filtersReducer(state.filters, action), 
+    todoList: todoListReducer(state.todoList, action), 
   }
 };
 
