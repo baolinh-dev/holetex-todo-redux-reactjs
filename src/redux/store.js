5ed911d60625de0e470afbf981141abdf91a9f37
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2024-02-28 02:31:47 +0700

    rename rootReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import rootReducer from "./reducer"; 
+import rootReducer from "./rootReducer"; 
 import { composeWithDevTools } from "redux-devtools-extension";  
 
 const composedEnhancers = composeWithDevTools();
