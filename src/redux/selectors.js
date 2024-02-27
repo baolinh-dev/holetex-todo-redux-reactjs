0e4dcad25609cda019d31b24fa61e2c45e0544c9
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2024-02-28 02:14:30 +0700

    selectors reselector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,14 @@
-export const todoListSelector = (state) => {
-  const todoRemaining = state.todoList.filter((todo) => {
-    return todo.name.includes(state.filters.search);
-  }); 
-  return todoRemaining
-};
+import { createSelector } from "reselect";
+
+export const todoListSelector = (state) => state.todoList;
 export const searchTextSelector = (state) => state.filters.search;
+
+export const todosRemainingSelector = createSelector(
+  todoListSelector,
+  searchTextSelector,
+  (todoList, searchText) => { 
+    return todoList.filter((todo) => {
+        return todo.name.includes(searchText)
+    })
+  }
+);
