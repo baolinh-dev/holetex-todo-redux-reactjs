710ea92683e684ad8723007201eb49681541c952
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2024-02-29 22:43:04 +0700

    alternative of rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import filtersReducer from "../components/Filters/FiltersSlice";
 import todoListReducer from "../components/Todo/TodosSlice";
 
-const rootReducer = combineReducers({ 
-  filters: filtersReducer, 
-  todoList: todoListReducer, 
-})
+const rootReducer = (state = {}, action) => {
+  return {
+    filters: filtersReducer(state.filters, action),
+    todoList: todoListReducer(state.todoList, action),
+  };
+};
 
 export default rootReducer;
